4ebfb5988c867f5c73561dd3a1cfc9e5a873a3e5
aweng126 <13075313106@163.com>
2017-07-03 08:55:48 +0800

    基本数据爬取成功，缺少招聘人数

--- recruitment/items.py
+++ recruitment/items.py
@@ -11,11 +11,15 @@ import scrapy
 class ZhilianItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    #公司名称
+    gsmz=scrapy.Field()
     #职位名称
     zwmc=scrapy.Field()
+    #招聘人数
+    zprs=scrapy.Field()
     #职位月薪
     zwyx= scrapy.Field()
     #公司地点
     gsdd = scrapy.Field()
-    #招聘数量
-    zprs=scrapy.Field()
+    #招聘要求
+    zpyq=scrapy.Field()
--- recruitment/spiders/zhilian.py
+++ recruitment/spiders/zhilian.py
@@ -19,31 +19,37 @@ class ZhilianSpider(scrapy.Spider):
     def start_requests(self):
         yield  Request(self.zhilian_url, self.parse_zhilian)
 
-    def parse_zhilian(self,response):
+    def parse_zhilian(self, response):
         lists=response.css('.newlist')
 
         for list in lists[1:]:
             item = ZhilianItem()
-            zwmc = list.css('.zwmc  div a::text').extract_first() #职位名称z
+            zwmc = list.css('.zwmc  div a::text').extract_first() #职位名称
             zwyx = list.css('.zwyx::text').extract_first()   # 职位月薪
             gsdd = list.css('.gzdd::text').extract_first()  # 公司地点
 
             # 招聘数量 (这里是一个url,想要的招聘人数在这个网址中)
-            # zprs = list.css('.zwmc div a::attr(href)')
-            item['zwyx'] = zwyx
-            item['zwmc'] = zwmc
-            item['gsdd'] = gsdd
-            print(self.num, item)
+            zprs_url = list.css('.zwmc div a::attr(href)').extract_first()
+            yield Request(url=zprs_url,callback=self.parse_zprs)
+
+            # print(self.num, zprs_url)
+
+            # item['zwyx'] = zwyx
+            # item['zwmc'] = zwmc
+            # item['gsdd'] = gsdd
+            # item['zprs']
+            # print(self.num, item)
 
             self.num = self.num+1
             yield item
 
-        next_page = response.css('.pagesDown-pos  a::attr(href)').extract_first()
+        # next_page = response.css('.pagesDown-pos  a::attr(href)').extract_first()
         # print('1111111111', next_page)
 
-        if next_page:
-            yield Request(url=next_page, callback=self.parse_zhilian)
+        # if next_page:
+        #     yield Request(url=next_page, callback=self.parse_zhilian)
 
-
+    def parse_zprs(self, response):
+        print('1111111111', response.text)
 
 
